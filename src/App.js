a537ffd3c0a9209a248c7d8a2a1fc66f2b633f04
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-04-25 15:34:33 +0100

    Updated path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './Home';
 import Edit from './Edit';
 import View from './View';
 import Breakfast from './Breakfast';
-import newEntry from './NewEntry';
+import NewEntry from './NewEntry';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // Created a function called 'App' that returns 'Router', 'Routes' and 'Route'. 
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='newEntry' element={<newEntry />} />
+          <Route path='NewEntry' element={<NewEntry />} />
           <Route path='edit' element={<Edit />} />
           <Route path='view' element={<View />} />
           <Route path='breakfast' element={<Breakfast />} />
