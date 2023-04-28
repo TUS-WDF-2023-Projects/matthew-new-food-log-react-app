141dfbc4be43f06e9de6af1a10ba49b15706272e
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-04-28 13:05:46 +0100

    created an update form

---
+++ src/MealEdit.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MealEdit = () => {
+    return (
+        <form>
+            <label>Fruit</label>
+            <input type="text" name="fruit" placeholder="Enter Fruit"></input><br />
+            <label>Grains</label>
+            <input type="text" name="grains" placeholder="Enter Grains"></input><br />
+            <label>Drink</label>
+            <input type="text" name="drink" placeholder="Enter Drink"></input><br />
+            <button type="button">Update</button>
+        </form>
+
+    )
+}
+
+export default MealEdit
+
+
+
