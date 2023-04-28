63c275cfb847b2bdf1b3962d1382427a8b2aef09
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-04-28 10:40:29 +0100

    Updated the Breakfast Component

--- src/Breakfast.js
+++ src/Breakfast.js
@@ -4,21 +4,12 @@ const Breakfast = () => {
 
     return (
         <div class="container-header">
-            <h1>Breakfast</h1>
-            <nav>
-                <li>Time:</li>
-                <li>Date:</li>
-            </nav>
-            <main>
-                <div class="container-main">
-                    <h2>About:</h2>
-                    <p>
-                        <li>Toast</li>
-                        <li>Orange Juice</li>
-                        <li>Banana</li>
-                    </p>
-                </div>
-            </main>
+            <h2> Today's Breakfast</h2>
+            <ul className='breakfast-menu'>
+                <li>Toast</li>
+                <li>Banana</li>
+                <li>Orange Juice</li>
+            </ul>
         </div>
     )
 }
