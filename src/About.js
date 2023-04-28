a2007080bb3e4a8d0ccd0ab3bbf7b9f047945fe9
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-04-28 10:14:18 +0100

    Added About Component

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <p>Welcome to my food log. Here is where I can keep track of my daily meals to make sure I'm keeping on the right track and not eating too much rubbish!</p>
+        </div>
+    )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import Edit from './Edit';
 import View from './View';
 import Breakfast from './Breakfast';
 import NewEntry from './NewEntry';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import About from './About';
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink, BrowserRouter } from 'react-router-dom';
 
 // Created a function called 'App' that returns 'Router', 'Routes' and 'Route'. 
 function App() {
   return (
     <div className="App">
       <Router>
+        <header>
+          <h1>My Food Log</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </header>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='NewEntry' element={<NewEntry />} />
           <Route path='edit' element={<Edit />} />
           <Route path='view' element={<View />} />
           <Route path='breakfast' element={<Breakfast />} />
+          <Route path='about' element={<About />} />
+          <Route path='navbar' element={<Navbar />} />
         </Routes>
       </Router>
     </div>
+
   );
 }
 
