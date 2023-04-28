4f9a92bc8a2d5577869a9994593a488237e3eeec
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-04-28 12:03:10 +0100

    Changed date format

--- src/Meals.js
+++ src/Meals.js
@@ -3,25 +3,25 @@ const Meals = [
         id: "1",
         meal: "Breakfast",
         time: "08:00",
-        date: "25-04-2023"
+        date: "2023-04-25"
     },
     {
         id: "2",
         meal: "Snack",
         time: "01:00",
-        date: "25-04-2023"
+        date: "2023-04-25"
     },
     {
         id: "3",
         meal: "Lunch",
         time: "01:00",
-        date: "25-04-2023"
+        date: "2023-04-25"
     },
     {
         id: "4",
         meal: "Dinner",
         time: "05:00",
-        date: "25-04-2023"
+        date: "2023-04-25"
     },
 ]
 
