8b28dcda4e9e87ed5da3f3e8419ab0aded5fdf31
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-04-28 13:05:30 +0100

    Replaced content with a table

--- src/Breakfast.js
+++ src/Breakfast.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import { Table } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Breakfast = () => {
-
     return (
         <div class="container-header">
-            <h2> Today's Breakfast</h2>
-            <ul className='breakfast-menu'>
-                <li>Toast</li>
-                <li>Banana</li>
-                <li>Orange Juice</li>
-            </ul>
+            <h2> Today's Breakfast</h2><br />
+            <Table stripped variant='dark' className='breakfast-menu'>
+                <thead>
+                    <tr>
+                        <th>Fruit</th>
+                        <th>Grains</th>
+                        <th>Drink</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td>Banana</td>
+                    <td>Toast</td>
+                    <td>Orange Juice</td>
+                </tbody>
+            </Table>
+            <td><Link to="/mealedit"><button id="edit-button">Edit Meal</button></Link></td>
         </div>
     )
 }
