d03d370b83e82bf84b994894694ec3b286443541
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-04-28 11:25:15 +0100

    Created List Items in Navbar

--- src/App.js
+++ src/App.js
@@ -15,8 +15,12 @@ function App() {
       <Router>
         <header>
           <h1>My Food Log</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
+          <Navbar>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </Navbar>
         </header>
         <Routes>
           <Route path='/' element={<Home />} />
