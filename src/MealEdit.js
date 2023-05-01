0afd822064680c301352a4aac79d4969711c10c8
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-05-01 12:05:39 +0100

    Re-wrote new code for 'Breakfast.js' component
    1. Created a function called breakfast
    2. Added a state variable called EditItem
    3. Added code to update an item in a collection of data.
    4. Returned a table of data that uses the .map method

--- src/MealEdit.js
+++ src/MealEdit.js
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+// This code is used to edit a 'Meal' item. When 'onUpdate' is called it will be passed the 'uopdatedItem' which is then used to update the origional 'Item'.
+const MealEdit = ({ item, onUpdate, onCancel }) => {
+    const [updatedItem, setUpdatedItem] = useState(item);
 
-const MealEdit = () => {
+    // This code is used to handle changes to input elements in a form. 
+    const handleChange = (e) => {
+        setUpdatedItem({ ...updatedItem, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onUpdate(item.id, updatedItem);
+    };
+
     return (
-        <form>
-            <label>Fruit</label>
-            <input type="text" name="fruit" placeholder="Enter Fruit"></input><br />
-            <label>Grains</label>
-            <input type="text" name="grains" placeholder="Enter Grains"></input><br />
-            <label>Drink</label>
-            <input type="text" name="drink" placeholder="Enter Drink"></input><br />
-            <button type="button">Update</button>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="id">ID:</label>
+            <input type="number" id="id" name="id" value={updatedItem.id} onChange={handleChange} />
+            <br />
+            <label htmlFor="fruit">Fruit</label>
+            <input type="text" id="fruit" name="fruit" value={updatedItem.fruit} onChange={handleChange} />
+            <br />
+            <label htmlFor="grains">Grains:</label>
+            <input type="text" id="grains" name="grains" value={updatedItem.grains} onChange={handleChange} />
+            <br />
+            <label htmlFor="drink">Drink:</label>
+            <input type="text" id="drink" name="drink" value={updatedItem.drink} onChange={handleChange} />
+            <br />
+            <button type="submit">Save</button>
+            <button type="button" onClick={onCancel}>Cancel</button>
         </form>
+    );
+};
 
-    )
-}
+export default MealEdit;
 
-export default MealEdit
 
 
 
