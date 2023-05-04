c78e4df6da17ab3af9c71e21661db236599eadaa
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-05-04 19:53:49 +0100

    Added FAQ's to about page
    Added a button to return home to home page

--- src/About.js
+++ src/About.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
         <div class="about-container">
-            <p>Welcome to my food log. Here is where I can keep track of my daily meals to make sure I'm keeping on the right track and not eating too much rubbish!</p>
+            <p>Welcome to my food log.<br /><br />
+
+                FAQ's<br /><br />
+
+                1. What is My Food Log?<br />
+
+                My Food log is a mobile application that helps users keep track of their daily meals.<br /><br />
+
+                2. How does it work?<br />
+
+                The app allows users to keep track of their daily meals using table formats. Users can update, delete and add new meal enteries. <br /><br />
+
+                3. Is it free to download?<br />
+
+                Yes, my food log is completly free for users to download.
+
+            </p>
+            <Link to="/"><button id="home-button">Home</button></Link>
         </div>
     )
 }
