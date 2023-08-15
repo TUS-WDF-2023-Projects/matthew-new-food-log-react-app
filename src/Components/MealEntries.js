3742b6f8d16c9edeb07b498ea3145d776118ac31
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-08-15 17:16:34 +0100

    Bootstrap
    - Imported Table From Bootstrap
    - Changed View From Link To Button

--- src/Components/MealEntries.js
+++ src/Components/MealEntries.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Table from 'react-bootstrap/Table';
 
 const MealEntries = ({ meals, onDelete }) => {
     const handleDelete = (e) => {
@@ -7,33 +8,36 @@ const MealEntries = ({ meals, onDelete }) => {
     };
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Meal</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Price</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {meals.map((item) => (
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.meal}</td>
-                        <td>{item.date}</td>
-                        <td>{item.time}</td>
-                        <td>{item.price}</td>
-                        <td>
-                            <Link to={`/meals/${item.id}`}>View</Link>
-                            <button className="btn btn-sm btn-danger" onClick={() => handleDelete(item.id)}>Delete</button>
-                        </td>
+        <div className="text-center">
+            <h1>Matthew's Food Log</h1><br />
+            <Table striped bordered>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Meal</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Price</th>
+                        <th>Actions</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {meals.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.meal}</td>
+                            <td>{item.date}</td>
+                            <td>{item.time}</td>
+                            <td>{item.price}</td>
+                            <td>
+                                <Link to={`/meals/${item.id}`}><button>View</button></Link>
+                                <button className="btn btn-sm btn-danger" onClick={() => handleDelete(item.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
     )
 };
 
