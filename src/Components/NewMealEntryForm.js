b327493a5bd1331d55a2bfdd9b121f5d939fd090
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-08-15 15:24:07 +0100

    Began Coding New Meal Entry Form
    Began coding the new meal entry form component

---
+++ src/Components/NewMealEntryForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewMealEntryForm = ({ onSubmitHandler }) => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let newMealEntry = {
+
+        }
+    };
+}
