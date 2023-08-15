a9df5fd7fbac938e71d14714c5a363daa3eb9e3f
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-08-15 14:08:26 +0100

    Created Data
    Created Data for ID, Meal, Date, Time and Price.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import './App.css';
 import React from 'react'
 
+const meals = [
+  {
+    id: 1,
+    meal: "Breakfast",
+    date: "15-08-2023",
+    time: "08:00 AM",
+    price: "6.50"
+  },
+  {
+    id: 2,
+    meal: "Lunch",
+    date: "15-08-2023",
+    time: "12:30 PM",
+    price: "15.99"
+  },
+  {
+    id: 3,
+    meal: "Snack",
+    date: "15-08-2023",
+    time: "03:30 PM",
+    price: "3.50"
+  },
+  {
+    id: 4,
+    meal: "Dinner",
+    date: "15-08-2023",
+    time: "07:00 PM",
+    price: "20.99"
+  }
+];
+
 const App = () => {
   return (
     <div>App Test</div>
