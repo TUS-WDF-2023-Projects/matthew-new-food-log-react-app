910b5c9e71ff5f88c8a206e28968c045cc4c17d3
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-08-21 11:20:09 +0100

    Button update
    Added bootstrap to button

--- src/Components/MealEntries.js
+++ src/Components/MealEntries.js
@@ -39,7 +39,7 @@ const MealEntries = ({ meals, onDelete }) => {
                     ))}
                 </tbody>
             </Table>
-            <Link to="/NewMealEntryForm"><button>Add New Entry</button></Link>
+            <Link to="/NewMealEntryForm"><button className="btn btn-sm btn-success">Add New Entry</button></Link>
         </div>
     )
 };
