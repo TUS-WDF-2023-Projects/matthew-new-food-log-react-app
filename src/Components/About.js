a118550fd34e93fdb5a4bd69a2215e07203ccb8c
MatthewCarey23 <123166634+MatthewCarey23@users.noreply.github.com>
2023-08-23 13:30:50 +0100

    About Information

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,21 @@
 import React from "react";
+import './App.css';
 
 const About = () => {
     return (
-        <div>
-            <h2>About:</h2>
+        <div className="about-info">
+            <h2>About:</h2><br />
+            <p>The purpose of this app is to provide a platform to those who wish to track and manage their daily meals.<br />
+                With intuitive features and a user-friendly design, users can keep track of their daily meals, time, date and price.</p><br />
+
+            <h2>Important!</h2>
+            <p>This app is currently under maintenance with the aim to add new features. These features will include:<br />
+                1. Monitoring Calorie Intake<br />
+                2. Meal Plans<br />
+                3. Daily Goals<br />
+                4. Progress Tracking<br />
+                5. Social Sharing</p>
+
         </div>
     )
 };
